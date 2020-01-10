8e684c57a676065c0ee9fe1eb7dd462574af5793
Sand038 <sandeepamadhusanka038@gmail.com>
2020-01-10 23:11:05 +0530

    Adding initial project

---
+++ SpringSecurityApp/src/main/java/com/sand/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.sand.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
+{
+}
---
+++ SpringSecurityApp/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Sand Company Home Page</title>
+</head>
+<body>
+    <h2>Sand Company Home Page</h2>
+    <hr>
+
+    Welcome to the Sand Company Home Page!
+</body>
+</html>
